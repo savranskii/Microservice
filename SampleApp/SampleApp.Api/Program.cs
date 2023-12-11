54f44d7374b989b0ac07248586b61f8da432146d
Taras <taras.savranskii@outlook.com>
2023-12-11 11:27:35 +0000

    Added rate limits

--- SampleApp/SampleApp.Api/Program.cs
+++ SampleApp/SampleApp.Api/Program.cs
@@ -1,6 +1,9 @@
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using SampleApp.Api.Infrastructure.ErrorHandlers;
 using SampleApp.Api.Infrastructure.Extensions;
+using SampleApp.Api.Infrastructure.Options;
 using SampleApp.Domain.Customer.Repositories;
 using SampleApp.Infrastructure.DbContexts;
 using SampleApp.Infrastructure.Models.Settings;
@@ -22,6 +25,18 @@ builder.Logging.AddSerilog(logger);
 builder.Host.UseSerilog();
 
 // Add services to the container.
+
+var rateLimitOptions = new RateLimitOptions();
+builder.Configuration.GetSection(RateLimitOptions.DefaultRateLimit).Bind(rateLimitOptions);
+builder.Services.AddRateLimiter(_ => _
+    .AddFixedWindowLimiter(policyName: RateLimitOptions.FixedPolicy, options =>
+    {
+        options.PermitLimit = rateLimitOptions.PermitLimit;
+        options.Window = TimeSpan.FromSeconds(rateLimitOptions.Window);
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = rateLimitOptions.QueueLimit;
+    }));
+
 builder.Services.AddExceptionHandler<DefaultExceptionHandler>();
 
 builder.Services.AddDbContext<CustomerContext>(opt => opt.UseInMemoryDatabase("CustomerDb"));
